4d7bcc6f629e35a41bdc40b8406f5f3c7c640b98
DinuwanB <dinubhagya97@gmail.com>
2019-12-14 16:31:26 +0530

    Icon changed + add Hero animation  and Navigators set up

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
+
+String id;
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
@@ -18,12 +23,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/chatLogo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
                 ),
                 Text(
-                  'Flash Chat',
+                  'Chat App',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
@@ -43,6 +54,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -61,6 +73,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
